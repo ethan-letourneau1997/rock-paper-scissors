365560a26268e32e7b8166b0bde12c713ff28c79
Ethan Letourneau <ethan.letourneau@snhu.edu>
2022-06-19 19:34:07 -0400

    Create function to play a round

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//computer's move
 let computerMove = "";
 
 function computerPlay(){
@@ -14,4 +15,29 @@ function computerPlay(){
     return computerMove;
 }
 
-console.log(computerPlay());
+
+// player's move
+function playRound(playerSelection,computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection === computerSelection){
+        console.log("It's a tie! " + playerSelection + " is the same as " + computerSelection + "!");
+    }
+    else if ((playerSelection == "rock" && computerSelection == "scissors")||
+    (playerSelection == "paper" && computerSelection == "rock")||
+    (playerSelection == "scissors" && computerSelection == "paper"))
+    {
+        console.log("You win! " + playerSelection + " beats " + computerSelection + "!");
+    }
+    else if ((playerSelection == "rock" && computerSelection == "paper")||
+    (playerSelection == "paper" && computerSelection == "scissors")||
+    (playerSelection == "scissors" && computerSelection == "rock"))
+    {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection + "!");
+    }
+    else{
+        console.log("Invalid entry! Please try again")
+    }
+}
+const playerSelection = "rock"
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
