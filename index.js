b7f458b9d89675c6fe2d0e493f6af6530af0e4e1
Ethan Letourneau <ethan.letourneau@snhu.edu>
2022-06-19 21:15:11 -0400

    Add five round match

--- index.js
+++ index.js
@@ -15,29 +15,68 @@ function computerPlay(){
     return computerMove;
 }
 
-
 // player's move
 function playRound(playerSelection,computerSelection){
     playerSelection = playerSelection.toLowerCase();
     if(playerSelection === computerSelection){
-        console.log("It's a tie! " + playerSelection + " is the same as " + computerSelection + "!");
+        return ("It's a tie! " + playerSelection + " is the same as " + computerSelection + "!");
     }
     else if ((playerSelection == "rock" && computerSelection == "scissors")||
     (playerSelection == "paper" && computerSelection == "rock")||
     (playerSelection == "scissors" && computerSelection == "paper"))
-    {
-        console.log("You win! " + playerSelection + " beats " + computerSelection + "!");
+    {   
+        return [("You win! " + playerSelection + " beats " + computerSelection + "!")];
     }
     else if ((playerSelection == "rock" && computerSelection == "paper")||
     (playerSelection == "paper" && computerSelection == "scissors")||
     (playerSelection == "scissors" && computerSelection == "rock"))
-    {
-        console.log("You lose! " + computerSelection + " beats " + playerSelection + "!");
+    {   
+        return [("You lose! " + computerSelection + " beats " + playerSelection + "!")];
     }
     else{
-        console.log("Invalid entry! Please try again")
+        return ("Invalid entry! Please try again")
     }
 }
-const playerSelection = "rock"
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+function game(){
+    for (i = 0; i < 5; i++){
+    let playerSelection = prompt("Select rock, paper, or scissors");
+    computerSelection = computerPlay();
+    if (playRound(playerSelection, computerSelection) == ("You lose! " + computerSelection + " beats " + playerSelection + "!")){
+        console.log(("You lose! " + computerSelection + " beats " + playerSelection + "!"));
+        computerPoints++;
+    }
+    else if (playRound(playerSelection, computerSelection) == ("You win! " + playerSelection + " beats " + computerSelection + "!")){
+        console.log(("You win! " + playerSelection + " beats " + computerSelection + "!"));
+        playerPoints++;
+    }
+    else{
+        console.log("It's a tie!!")
+    }
+    console.log("your score: " + playerPoints)
+    console.log("computer score: " + computerPoints)
+}
+    if(playerPoints > computerPoints){
+        console.log("FINAL SCORE")
+        console.log("your score: " + playerPoints)
+        console.log("computer score: " + computerPoints)
+        console.log("You win!")
+    }
+    else if(playerPoints < computerPoints){
+        console.log("FINAL SCORE")
+        console.log("your score: " + playerPoints)
+        console.log("computer score: " + computerPoints)
+        console.log("You lose!")
+    }
+    else{
+        console.log("FINAL SCORE")
+        console.log("your score: " + playerPoints)
+        console.log("computer score: " + computerPoints)
+        console.log("Its a tie!")
+    }
+}
+game();
+
