ad47e243a0905b147b469d83ee94ffb985b3a61e
Ethan Letourneau <ethan.letourneau@snhu.edu>
2022-06-20 23:44:40 -0400

    set functions sans check score

--- index.js
+++ index.js
@@ -17,34 +17,86 @@ function computerPlay(){
 
 // player's move
 function playRound(playerSelection,computerSelection){
-    playerSelection = playerSelection.toLowerCase();
+    
     if(playerSelection === computerSelection){
-        return ("It's a tie! " + playerSelection + " is the same as " + computerSelection + "!");
+        console.log(("It's a tie! " + playerSelection + " is the same as " + computerSelection + "!"));
+        results.textContent = ("It's a tie! " + playerSelection + " is the same as " + computerSelection + "!");
     }
     else if ((playerSelection == "rock" && computerSelection == "scissors")||
     (playerSelection == "paper" && computerSelection == "rock")||
     (playerSelection == "scissors" && computerSelection == "paper"))
     {   
-        return [("You win! " + playerSelection + " beats " + computerSelection + "!")];
+        console.log(("You win! " + playerSelection + " beats " + computerSelection + "!"));
+        playerScore += 1;
+        results.textContent = [("You win! " + playerSelection + " beats " + computerSelection + "!")];
     }
     else if ((playerSelection == "rock" && computerSelection == "paper")||
     (playerSelection == "paper" && computerSelection == "scissors")||
     (playerSelection == "scissors" && computerSelection == "rock"))
     {   
-        return [("You lose! " + computerSelection + " beats " + playerSelection + "!")];
-    }
-    else{
-        return ("Invalid entry! Please try again")
+        console.log(("You lose! " + computerSelection + " beats " + playerSelection + "!"));
+        computerScore +=1;
+        results.textContent = [("You lose! " + computerSelection + " beats " + playerSelection + "!")];
     }
 }
+// ------------------------------------------------
 
+// rock event listener
+const button1 = document.getElementById('button1');
+button1.addEventListener("click", rockFunction);
+
+function rockFunction(){
+    console.log('rock');
+    playRound('rock',computerPlay());
+    pScore.textContent = ("player: " + playerScore);
+    cScore.textContent =("computer: " + computerScore);
+    // game('rock',computerPlay());
+}
+
+//paper event listener
+const button2 = document.getElementById('button2');
+button2.addEventListener("click", paperFunction);
+
+function paperFunction(){
+    console.log('paper');
+    playRound('paper',computerPlay());
+    pScore.textContent = ("player: " + playerScore);
+    cScore.textContent =("computer: " + computerScore);
+}
+
+//scissor event listener
+const button3 = document.getElementById('button3');
+button3.addEventListener("click", scissorFunction);
+
+function scissorFunction(){
+    console.log('scissor');
+    playRound('scissors',computerPlay());
+    pScore.textContent = ("player: " + playerScore);
+    cScore.textContent =("computer: " + computerScore);
+}
+// -----------------------------------------------
+
+// initialize scoreboard
+let playerScore = 0;
+let computerScore = 0;
+
+
+// ------------------------------------------------
+
+// change text 
+const results = document.getElementById('round-results');
+const pScore = document.getElementById('player-score');
+const cScore = document.getElementById('computer-score');
+
+
+// ------------------------------------------------
 let playerPoints = 0;
 let computerPoints = 0;
 
 function game(){
     for (i = 0; i < 5; i++){
-    let playerSelection = prompt("Select rock, paper, or scissors");
-    computerSelection = computerPlay();
+    // let playerSelection = prompt("Select rock, paper, or scissors");
+    // computerSelection = computerPlay();
     if (playRound(playerSelection, computerSelection) == ("You lose! " + computerSelection + " beats " + playerSelection + "!")){
         console.log(("You lose! " + computerSelection + " beats " + playerSelection + "!"));
         computerPoints++;
@@ -78,5 +130,7 @@ function game(){
         console.log("Its a tie!")
     }
 }
-game();
+// game();
+
+
 
